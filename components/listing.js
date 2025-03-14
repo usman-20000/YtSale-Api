791e7338ff2d85177bb8d55c6cddd3962a51da3a
usman-20000 <usmanaliiali125@gmail.com>
2025-03-14 22:20:03 +0500

    msg

--- components/listing.js
+++ components/listing.js
@@ -1,6 +1,10 @@
 const mongoose = require('mongoose');
 
 const accountSchema = new mongoose.Schema({
+    userId:{
+        type: String,
+        required: true
+    },
     accountName: {
         type: String,
         required: true
